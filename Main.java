19134264f32a614dae70371a0ca939904ced6c6f
kraaken13 <128466825+kraaken13@users.noreply.github.com>
2023-07-26 10:23:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        byte num;
+        int num1;
+        short num2;
+        long num3;
+        float num4;
+        double num5;
+        boolean string;
+        String string1;
+        char symbol;
+        byte nums = -127;
+        short nums1 = 32000;
+        int nums2 = 5000000;
+        long nums3 = 900000000;
+        float nums4 = 1.5f;
+        double nums5 = 1.0005d;
+        char symbol1 = 1;
+        String text = "Hello \n world";
+        char tex = 123;
+        String str = "Yo";
+        float pi = 3.14f;
+        double NASA = 5.5555555555555d;
+
+
+    }
+}
